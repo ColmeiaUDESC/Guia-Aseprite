23c68c3ea6b189290b14e739358bd3cd3cb7f2c9
Brian Bertemes <61915030+MrBertemes@users.noreply.github.com>
2021-07-09 12:07:15 -0300

    Organização dos scripts

---
+++ Instaladores/AsepriteFedoraInstall-x86.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+echo -e "\e[1;33m
+
+  ____ ___  _     __  __ _____ ___    _    
+ / ___/ _ \| |   |  \/  | ____|_ _|  / \   
+| |  | | | | |   | |\/| |  _|  | |  / _ \  
+| |__| |_| | |___| |  | | |___ | | / ___ \ 
+ \____\___/|_____|_|  |_|_____|___/_/   \_\ 
+ 
+----------------------------------------------------
+ 
+ Github : https://github.com/ColmeiaUDESC
+ 
+----------------------------------------------------
+ 
+ All rights reserved to: https://github.com/aseprite
+ 
+----------------------------------------------------
+"
+
+
+cd ~/Downloads;
+wget https://github.com/aseprite/skia/releases/download/m81-b607b32047/Skia-Linux-Release-x86.zip;
+wget https://github.com/aseprite/aseprite/releases/download/v1.2.27/Aseprite-v1.2.27-Source.zip;
+sudo dnf install -y gcc-c++ cmake ninja-build libX11-devel libXcursor-devel libXi-devel mesa-libGL-devel fontconfig-devel
+cd ~ ;mkdir deps ;cd deps ;mkdir skia ;cd ~/Downloads ;sudo unzip Skia-Linux-Release-x86.zip -d ~/deps/skia
+cd ~; mkdir aseprite ;cd ~/Downloads ;sudo unzip Aseprite-v1.2.27-Source.zip -d ~/aseprite ;cd ~/aseprite; mkdir build ;cd build; cmake \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DLAF_BACKEND=skia \
+  -DSKIA_DIR=$HOME/deps/skia \
+  -DSKIA_LIBRARY_DIR=$HOME/deps/skia/out/Release-x86 \
+  -DSKIA_LIBRARY=$HOME/deps/skia/out/Release-x86/libskia.a \
+  -G Ninja \
+  ..
+ninja aseprite
+cd ~/Downloads ; 
+wget -A png https://raw.githubusercontent.com/ColmeiaUDESC/Guia-Aseprite/main/icon.png ;
+sudo mv icon.png ~/../../usr/share/icons/hicolor/48x48/apps ;cd ~/../../usr/share/applications/ ;
+sudo echo "[Desktop Entry]" >> aseprite.desktop ;
+sudo echo "Type=Application" >> aseprite.desktop ;
+sudo echo "Name=Aseprite" >> aseprite.desktop ;
+sudo echo "Exec=sh -c "~/aseprite/build/bin/./aseprite" " >> aseprite.desktop ;
+sudo echo "Icon=/usr/share/icons/hicolor/48x48/apps/aseprite.png" >> aseprite.desktop ;
+sudo echo "Terminal=false" >> aseprite.desktop
