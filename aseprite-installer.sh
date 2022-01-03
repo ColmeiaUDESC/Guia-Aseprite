72dd85306b18f87901657ef3fa2e278cef736384
Kawan Weege <47753585+DragonOfWar@users.noreply.github.com>
2022-01-03 14:54:22 -0300

    Add caminho instalacao (#38)
    * Adicionar parâmetro para indicar o diretório de instalação
    
    * Adicionar  faltantes
    
    * Permitir usar -f
    
    * Melhorar mensagem de desinstalação
    
    * Melhorar codigo do prompt de desinstalação
    
    * Arrumar erro na desinstalação
    
    * Melhorar verificação do diretório de instalação
    
    * Tornar o código mais consistente

--- aseprite-installer.sh
+++ aseprite-installer.sh
@@ -26,6 +26,29 @@ echo -e "\e[1;33m
 ----------------------------------------------------
 "
 
+# Processar argumentos para conseguir o diretorio padrao
+DIR_INSTALACAO=$HOME
+ARGS=$(getopt -a -n aseprite-installer.sh -o f: --long dir-instalacao: -- "$@")
+eval set -- "$ARGS"
+while :
+do
+  case "$1" in
+    -f | --dir-instalacao)
+      if [ -f $2 ]; then
+        echo "$2 é um arquivo. Abortando."
+        exit 1
+      fi
+      mkdir -p $2
+      DIR_INSTALACAO=$2
+      shift 2
+      ;;
+    --) 
+      shift
+      break
+      ;;
+  esac
+done
+
 # Checar a arquitetura e sistema operacional na qual estamos rodando
 OS_ARCH="$(uname -m)"
 command -v apt-get > /dev/null && OS_DISTRO="debian"
@@ -43,10 +66,10 @@ cd ~/Downloads
 [ $OS_ARCH = "x86" ] && { SKIA_ZIP="Skia-Linux-Release-x86.zip"; SKIA_RELEASE="Release-x86"; } 
 [ $OS_ARCH = "x86_64" ] && { SKIA_ZIP="Skia-Linux-Release-x64.zip";  SKIA_RELEASE="Release-x64"; }
 wget https://github.com/aseprite/skia/releases/download/m81-b607b32047/$SKIA_ZIP; mkdir -p ~/deps/skia; sudo unzip $SKIA_ZIP -d ~/deps/skia
-wget https://github.com/aseprite/aseprite/releases/download/v1.2.27/Aseprite-v1.2.27-Source.zip; mkdir -p ~/aseprite/build; sudo unzip Aseprite-v1.2.27-Source.zip -d ~/aseprite;
+wget https://github.com/aseprite/aseprite/releases/download/v1.2.27/Aseprite-v1.2.27-Source.zip; mkdir -p $DIR_INSTALACAO/aseprite/build; sudo unzip Aseprite-v1.2.27-Source.zip -d $DIR_INSTALACAO/aseprite;
 
 # Compilar o aseprite
-cd ~/aseprite/build
+cd $DIR_INSTALACAO/aseprite/build
 cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DLAF_BACKEND=skia \
@@ -70,7 +93,7 @@ rm $SKIA_ZIP
 DIR_SHARE=$HOME/.local/share
 DIR_ICONES=$DIR_SHARE/icons/hicolor
 ASEPRITE_DESKTOP=$DIR_SHARE/applications/aseprite.desktop
-cd $HOME/aseprite/build/bin/data/icons
+cd $DIR_INSTALACAO/aseprite/build/bin/data/icons
 mkdir -p $DIR_ICONES/16x16/apps && cp ase16.png $_/aseprite.png 
 mkdir -p $DIR_ICONES/32x32/apps && cp ase32.png $_/aseprite.png
 mkdir -p $DIR_ICONES/48x48/apps && cp ase48.png $_/aseprite.png
@@ -80,7 +103,7 @@ mkdir -p $DIR_ICONES/256x256/apps && cp ase256.png $_/aseprite.png
 echo "[Desktop Entry]" > $ASEPRITE_DESKTOP
 echo "Type=Application" >> $ASEPRITE_DESKTOP
 echo "Name=Aseprite" >> $ASEPRITE_DESKTOP
-echo "Exec=sh -c \"$HOME/aseprite/build/bin/aseprite\" " >> $ASEPRITE_DESKTOP
+echo "Exec=sh -c \"$DIR_INSTALACAO/aseprite/build/bin/aseprite\" " >> $ASEPRITE_DESKTOP
 echo "Icon=aseprite" >> $ASEPRITE_DESKTOP
 echo "Terminal=false" >> $ASEPRITE_DESKTOP
 echo "Categories=Graphics;2DGraphics;" >> $ASEPRITE_DESKTOP
